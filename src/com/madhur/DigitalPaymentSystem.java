9a1b80ecafe5ecffe578038ccd1125694c547b4a
Madhurmoms <madhurjatiya13@gmail.com>
2022-06-07 20:51:47 +0530

    final project

---
+++ src/com/madhur/DigitalPaymentSystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.madhur;
+
+/**
+ *
+ * @author 91940
+ */
+public class DigitalPaymentSystem {
+
+    public static void main(String[] args) {
+        HomePage.main(args);
+    }
+    
+}
